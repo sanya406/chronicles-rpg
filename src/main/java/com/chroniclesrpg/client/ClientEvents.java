01744f2d03c09a6ed8e0817704033cf7c1235e97
sanya406 <zeleznyiandrey@gmail.com>
2026-09-19 18:41:31 +0300

    Update ClientEvents.java

--- src/main/java/com/chroniclesrpg/client/ClientEvents.java
+++ src/main/java/com/chroniclesrpg/client/ClientEvents.java
@@ -1,37 +1,27 @@
 package com.chroniclesrpg.client;
 
 import com.chroniclesrpg.ChroniclesRpg;
-import com.mojang.blaze3d.platform.InputConstants;
-import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import com.chroniclesrpg.client.screen.CharacterScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(modid = ChroniclesRpg.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = net.minecraftforge.api.distmarker.Dist.CLIENT)
-public final class ClientEvents {
-    public static final KeyMapping OPEN_STATS = new KeyMapping(
-            "key.chroniclesrpg.open_stats",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_K,
-            "key.categories.chroniclesrpg"
-    );
-
-    private ClientEvents() {}
+@Mod.EventBusSubscriber(modid = ChroniclesRpg.MODID, value = Dist.CLIENT)
+public class ClientEvents {
 
     @SubscribeEvent
-    public static void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(OPEN_STATS);
+    public static void onKeyInput(net.minecraftforge.client.event.InputEvent.Key event) {
+        if (KeyBindings.CHARACTER_SCREEN_KEY.consumeClick()) {
+            Minecraft.getInstance().setScreen(new CharacterScreen());
+        }
     }
 
-    @Mod.EventBusSubscriber(modid = ChroniclesRpg.MODID, value = net.minecraftforge.api.distmarker.Dist.CLIENT)
-    public static class GameEvents {
-        @SubscribeEvent
-      TickEvent.ClientTickEvent event
-            if (OPEN_STATS.consumeClick() && net.minecraft.client.Minecraft.getInstance().screen == null) {
-                net.minecraft.client.Minecraft.getInstance().setScreen(new RpgScreen());
-            }
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            // Дополнительная логика тика клиента
         }
     }
 }
