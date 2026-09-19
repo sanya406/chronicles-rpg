e4a96651ef169cb0714061b90c71950382f12e5c
sanya406 <zeleznyiandrey@gmail.com>
2026-09-19 19:13:49 +0300

    Update ChroniclesRpg.java

--- src/main/java/com/chroniclesrpg/ChroniclesRpg.java
+++ src/main/java/com/chroniclesrpg/ChroniclesRpg.java
@@ -1,19 +1,31 @@
-package com.chroniclesrpg.client;
+package com.chroniclesrpg;
 
-import com.chroniclesrpg.ChroniclesRpg;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.SimpleChannel;
+import net.minecraft.resources.ResourceLocation;
 
-@Mod.EventBusSubscriber(modid = ChroniclesRpg.MODID, value = Dist.CLIENT)
-public class ClientEvents {
+@Mod(ChroniclesRpg.MODID)
+public class ChroniclesRpg {
+    public static final String MODID = "chroniclesrpg";
+    private static final String PROTOCOL_VERSION = "1";
 
-    @SubscribeEvent
-    public static void onClientTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            // Логика тика клиента
-        }
+    public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(
+        new ResourceLocation(MODID, "main"),
+        () -> PROTOCOL_VERSION,
+        PROTOCOL_VERSION::equals,
+        PROTOCOL_VERSION::equals
+    );
+
+    public ChroniclesRpg() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Инициализация мода
     }
 }
