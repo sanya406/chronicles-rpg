a3fd944f82991a5aa37542f97fd02485174f83d5
sanya406 <zeleznyiandrey@gmail.com>
2026-09-19 19:15:00 +0300

    Update ClientEvents.java

--- src/main/java/com/chroniclesrpg/client/ClientEvents.java
+++ src/main/java/com/chroniclesrpg/client/ClientEvents.java
@@ -1,8 +1,6 @@
 package com.chroniclesrpg.client;
 
 import com.chroniclesrpg.ChroniclesRpg;
-import com.chroniclesrpg.client.screen.CharacterScreen;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,16 +10,9 @@ import net.minecraftforge.fml.common.Mod;
 public class ClientEvents {
 
     @SubscribeEvent
-    public static void onKeyInput(net.minecraftforge.client.event.InputEvent.Key event) {
-        if (KeyBindings.CHARACTER_SCREEN_KEY.consumeClick()) {
-            Minecraft.getInstance().setScreen(new CharacterScreen());
-        }
-    }
-
-    @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
-            // Дополнительная логика тика клиента
+            // Логика тика клиента
         }
     }
 }
