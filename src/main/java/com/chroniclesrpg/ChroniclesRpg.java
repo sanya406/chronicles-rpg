dc3b7d06cb9da72ec9ac812719fdee12f2740b15
sanya406 <zeleznyiandrey@gmail.com>
2026-09-19 19:01:25 +0300

    Update ChroniclesRpg.java

--- src/main/java/com/chroniclesrpg/ChroniclesRpg.java
+++ src/main/java/com/chroniclesrpg/ChroniclesRpg.java
@@ -1,43 +1,19 @@
-package com.chroniclesrpg;
+package com.chroniclesrpg.client;
 
-import com.chroniclesrpg.client.ClientEvents;
-import com.chroniclesrpg.command.RpgCommands;
-import com.chroniclesrpg.data.RpgData;
-import com.chroniclesrpg.network.NetworkHandler;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import com.chroniclesrpg.ChroniclesRpg;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.simple.SimpleChannel;
-import net.minecraftforge.network.NetworkRegistry;
 
-@Mod(ChroniclesRpg.MODID)
-public class ChroniclesRpg {
-    public static final String MODID = "chroniclesrpg";
-    public static final String PROTOCOL = "1";
+@Mod.EventBusSubscriber(modid = ChroniclesRpg.MODID, value = Dist.CLIENT)
+public class ClientEvents {
 
-    public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation(MODID, "main"),
-            () -> PROTOCOL, PROTOCOL::equals, PROTOCOL::equals
-    );
-
-    public ChroniclesRpg() {
-        NetworkHandler.register();
-        MinecraftForge.EVENT_BUS.register(new CommonEvents());
-        MinecraftForge.EVENT_BUS.register(ServerTickHandler.class);
-    }
-
-    public static class CommonEvents {
-        @SubscribeEvent
-        public void registerCommands(RegisterCommandsEvent event) {
-            RpgCommands.register(event.getDispatcher());
-        }
-
-        @SubscribeEvent
-        public void clone(PlayerEvent.Clone event) {
-            RpgData.copyFrom(event.getOriginal(), event.getEntity());
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            // Логика тика клиента
         }
     }
 }
